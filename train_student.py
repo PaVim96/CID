b3d25d6eb90859f53c8246e3261d36e349e53728
PaVim96 <Patrick-Vimr@gmx.de>
2023-05-29 19:28:41 +0200

    train script extended

--- train_student.py
+++ train_student.py
@@ -24,7 +24,8 @@ from CID.helper.util import adjust_learning_rate
 from CID.distiller_zoo import KL, Cosine
 from CID.distiller_zoo import NORM_MSE
 
-from CID.helper.loops import train_distill as train_init, validate, validate_st, train_distill_context as train
+from CID.helper.loops import train_no_int
+from CID.helper.loops import train_distill as train_init, validate, validate_st, validate_st_no_int,train_distill_context as train
 from CID.helper.util import set_seed
 
 
@@ -60,7 +61,7 @@ def parse_option():
     parser.add_argument('--path_t', type=str, default=None, help='teacher model')
     
     # distillation
-    parser.add_argument('--distill', type=str, default='CID', choices=['CID'])
+    parser.add_argument('--use_int', type=bool, default=True)
     parser.add_argument('--trial', type=str, default='1', help='trial id')
 
     parser.add_argument('-a', '--aa', type=float, default=1, help='weight for classification')
@@ -93,7 +94,7 @@ def parse_option():
 
     opt.model_t = get_teacher_name(opt.path_t)
 
-    opt.model_name = 'S:{}_{}_{}_a:{}_b:{}_c:_{}{}'.format(opt.model_s, opt.dataset, opt.distill,
+    opt.model_name = 'S:{}_{}_{}_a:{}_b:{}_c:_{}{}'.format(opt.model_s, opt.dataset, opt.use_int,
                         opt.aa, opt.bb, opt.cc, opt.trial)
 
     opt.save_folder = os.path.join(opt.model_path, opt.model_name)
@@ -116,17 +117,155 @@ def load_teacher(model_path, n_cls):
     print('==> loading teacher model')
     model_t = get_teacher_name(model_path)
     model = model_dict[model_t](num_classes=n_cls)
-    model.load_state_dict(torch.load(model_path)['model'])
+    if torch.cuda.is_available():
+        model.load_state_dict(torch.load(model_path)['model'])
+    else: 
+        model.load_state_dict(torch.load(model_path, map_location="cpu")["model"])
     print('==> done')
     return model
 
+def main_train_no_int(opt):
+    best_acc = 0
 
-def main():
+    #opt = parse_option()
+    
+    print(opt)
+    
+    set_seed(opt.seed)
+
+
+    # dataloader
+    if opt.dataset == 'cifar100':
+  
+        train_loader, val_loader, n_data = get_cifar100_dataloaders(batch_size=opt.batch_size,
+                                                                    num_workers=opt.num_workers,
+                                                                    is_instance=True)
+        n_cls = 100
+    else:
+        raise NotImplementedError(opt.dataset)
+
+    # model
+    model_t = load_teacher(opt.path_t, n_cls)
+    model_s = model_dict[opt.model_s](num_classes=n_cls)
+
+    data = torch.randn(2, 3, 32, 32)
+    model_t.eval()
+    model_s.eval()
+    feat_t, _ = model_t(data, is_feat=True)
+    feat_s, _= model_s(data, is_feat=True)
+
+    module_list = nn.ModuleList([])
+    module_list.append(model_s)
+    
+    trainable_list = nn.ModuleList([])
+    trainable_list2 = nn.ModuleList([])
+    
+    trainable_list.append(model_s)
+
+    criterion_cls = nn.CrossEntropyLoss()
+    criterion_mse = NORM_MSE()
+
+    criterion_cc = Cosine() 
+
+    _, Cs_h = feat_s[opt.hint_layer].shape
+    _, Ct_h = feat_t[opt.hint_layer].shape
+        
+    #
+    Reger_fea = Reg( Cs_h, Ct_h)
+        
+    module_list.append(Reger_fea)
+        
+    trainable_list2.append(Reger_fea)
+
+
+    criterion_list = nn.ModuleList([])
+    criterion_list.append(criterion_cls)
+    criterion_list.append(criterion_mse)
+    criterion_list.append(criterion_cc)
+
+
+    # optimizer
+    optimizer = optim.SGD([{'params': trainable_list.parameters()}, {'params': trainable_list2.parameters(), 'weight_decay': 0.0}],
+                          lr=opt.learning_rate,
+                          momentum=opt.momentum,
+                          weight_decay=opt.weight_decay,
+                          nesterov=True)
+
+    # append teacher after optimizer to avoid weight_decay
+    module_list.append(model_t)
+
+    if torch.cuda.is_available():
+        module_list.cuda()
+        criterion_list.cuda()
+        cudnn.benchmark = True
+
+    # validate teacher accuracy
+    teacher_acc, _, _ = validate(val_loader, model_t, criterion_cls, opt)
+    print('teacher accuracy: ', teacher_acc)
+
+
+
+    # routine
+    for epoch in range(1, opt.epochs + 1):
+
+        adjust_learning_rate(epoch, opt, optimizer)
+        print("==> training...")
+
+        time1 = time.time()
+        
+        train_acc, train_loss = train_no_int(epoch, train_loader, module_list, criterion_list, 
+                                             optimizer, opt)
+        
+        if train_loss!=train_loss:
+            return
+        
+        time2 = time.time()
+        print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
+
+      
+        test_acc, tect_acc_top5 = validate_st_no_int(val_loader, model_s, criterion_cls)
+
+
+        
+        if test_acc > best_acc:
+            best_acc = test_acc
+            state = {
+                'epoch': epoch,
+                'model': model_s.state_dict(),
+                'best_acc': best_acc,
+            }
+            save_file = os.path.join(opt.save_folder, '{}_best.pth'.format(opt.model_s))
+            print('saving the best model!')
+            torch.save(state, save_file)
+
+        # regular saving
+        if epoch % opt.save_freq == 0:
+            print('==> Saving...')
+            state = {
+                'epoch': epoch,
+                'model': model_s.state_dict(),
+                'accuracy': test_acc,
+            }
+            save_file = os.path.join(opt.save_folder, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
+            torch.save(state, save_file)
+
+    # This best accuracy is only for printing purpose.
+    print('best accuracy:', best_acc.cpu().numpy())
+
+    # save model
+    state = {
+        'opt': opt,
+        'model': model_s.state_dict(),
+    }
+    save_file = os.path.join(opt.save_folder, '{}_last.pth'.format(opt.model_s))
+    torch.save(state, save_file)
+
+def main_normal_train(opt):
     
     
     best_acc = 0
 
-    opt = parse_option()
+    #opt = parse_option()
     
     print(opt)
     
@@ -164,7 +303,7 @@ def main():
     criterion_cls = nn.CrossEntropyLoss()
     criterion_div = KL(opt.net_T)
 
-    if opt.distill == 'CID':
+    if opt.use_int:
         
         criterion_kd = NORM_MSE()
         
@@ -177,7 +316,6 @@ def main():
         #TODO: remove, just for debugging and understanding purposes
         print(Cs_h)
         print(Cs_h * 2)
-        import ipdb; ipdb.set_trace()
         model_s_fc_new = Reg( Cs_h*2, n_cls)
         
         module_list.append(model_s_fc_new)
@@ -294,4 +432,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    opt = parse_option()
+    if opt.use_int:
+        main_normal_train(opt)
+    else: 
+        main_train_no_int(opt)
